73701cdc14f27ed0fffbf4cfab3a333efeda04d4
ubehruz <behruzusmonov94@gmail.com>
2021-04-09 19:18:49 +0500

    check qilish qushildi

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const todos = document.querySelector('.todos')
 
 
 
-
+//add todo
 const generateTodo = (todo,time) => {
     //get time
     var time = new Date()
@@ -14,11 +14,11 @@ const generateTodo = (todo,time) => {
     let html = `
     <label class="list-group-item d-flex justify-content-between align-items-center">
     <div class="d-flex">
-        <input class="form-check-input me-1" type="checkbox" value="">
+        <input class="form-check-input me-1 check" type="checkbox" value="">
         <p class="mx-2">${todo}</p>
         <p class="fst-italic date">${hours}:${minutes}:${sec}</p>
     </div>
-    <button class="btn btn-danger btn-sm" id="delete-todo"><i class="fas fa-trash-alt"></i></button>
+    <button class="delete btn btn-danger btn-sm fas fa-trash-alt"></button>
     </label>
     `
     return html;
@@ -31,4 +31,29 @@ addForm.addEventListener('submit', e => {
     todos.innerHTML += generateTodo(inpValue)
     addForm.reset()
 
+})
+
+
+//delete todos
+
+const deleteItem = document.getElementById('delete-todo')
+
+todos.addEventListener('click', (e) => {
+    if(e.target.classList.contains('delete')){
+        let conf = confirm('Siz rostan ham o`chirmoqchimisiz?')
+        if(conf){
+            e.target.parentElement.remove()
+        }
+        
+    }
+})
+
+//checked todo
+
+todos.addEventListener('click', (e)=>{
+    if(e.target.classList.contains('check')){
+        e.target.setAttribute('checked', 'checked')
+        e.target.parentElement.parentElement.classList.toggle('checked-item')
+    }
+    
 })
