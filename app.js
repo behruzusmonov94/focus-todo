819c639898269678724575dcf8e809415a50bf01
ubehruz <behruzusmonov94@gmail.com>
2021-04-10 17:07:54 +0500

    localStorage qushildi

--- app.js
+++ app.js
@@ -28,9 +28,10 @@ addForm.addEventListener('submit', e => {
     e.preventDefault();
     let inpValue = addForm.name.value.trim()
     if(inpValue.length){
-        console.log(inpValue);
         todos.innerHTML += generateTodo(inpValue)
         addForm.reset()
+        var time = Date.now()
+        localStorage.setItem(time, inpValue)
     }
     
 
@@ -65,4 +66,36 @@ todos.addEventListener('click', (e)=>{
 })
 
 
+//generate with localStorage
+//add todo
+const generateTodoLocalStorage = (name,hours,minutes) => {
+    //get time
+    var time = new Date()
+    var hours = time.getHours()
+    var minutes = time.getMinutes()
+    var sec = time.getSeconds()
+    let html = `
+    <label class="list-group-item d-flex justify-content-between align-items-center">
+    <div class="d-flex">
+        <input class="form-check-input me-1 check" type="checkbox" value="">
+        <p class="mx-2">${name}</p>
+        <p class="fst-italic date">${hours}:${minutes}</p>
+    </div>
+    <button class="delete btn btn-sm text-danger fas fa-trash-alt" data-bs-toggle="modal" data-bs-target="#deleteTodos"></button>
+    </label>
+    `
+    return html;
+}
 
+
+for(let i = 0; i < localStorage.length; i++){
+    let dataKey = localStorage.key(i)
+    let dataVal = localStorage.getItem(dataKey)
+    let getDate = new Date(parseInt(dataKey))
+    let getHours = getDate.getHours()
+    let getMinutes = getDate.getMinutes()
+
+    todos.innerHTML += generateTodoLocalStorage(dataVal, getHours, getMinutes)
+    
+    
+}
